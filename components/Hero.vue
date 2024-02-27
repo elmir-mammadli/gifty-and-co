4a424ab7eecd72a7efa2ac79e210deae286a6469
Elmir Mammadli <101706322+elmir-mammadli@users.noreply.github.com>
2024-02-27 22:50:32 +0100

    Refactor Hero and Navbar components

--- components/Hero.vue
+++ components/Hero.vue
@@ -8,7 +8,7 @@
         data-aos="fade-up"
         data-aos-once="true"
         data-aos-duration="600"
-        class="bg-[#FF0060] text-white px-2 py-3.5 -rotate-2 mt-2">bloom with joy.</h1>
+        class="bg-[#FF0060] text-white px-2 py-3.5 -rotate-2 mt-2 rounded-md">bloom with joy.</h1>
         <p class="font-light text-gray-600 text-[20px] mt-4">The best presents bring smiles that never fade.</p>
        </article> 
     </div>
--- components/navigation/routesData.ts
+++ components/navigation/routesData.ts
@@ -1,26 +1,32 @@
 export interface NavbarData {
     label: string;
     path: string;
+    color: string;
 }
 export const routesData: NavbarData[] = [
     {
         label: 'Shop by Interest',
-        path: '/'
+        path: '/',
+        color: '#43B17C'
     },
     {
         label: 'Occasions',
-        path: '/about'
+        path: '/about',
+        color: '#0A95FF'
     },
     {
-        label: 'For',
-        path: '/contact'
+        label: 'Gift Ideas For',
+        path: '/contact',
+        color: '#FF5733'
     },
     {
         label: 'Trending',
-        path: '/blog'
+        path: '/blog',
+        color: '#FC6C85'
     },
     {
         label: 'Sale',
-        path: '/blog2'
+        path: '/blog2',
+        color: '#E40A2D'
     }
 ]
