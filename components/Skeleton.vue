1a56e6994365e53558366b7bae309e4784850520
Elmir Mammadli <elmirmammadli18@gmail.com>
2024-12-01 21:48:03 -0500

    refactor Skeleton component: adjust layout dimensions and improve structure

--- components/Skeleton.vue
+++ components/Skeleton.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="block max-w-max">
-        <div class="w-full h-full bg-gray-200/75 rounded-lg"/>
-        <div class="flex mt-2 justify-between gap-x-2">
-            <div class="flex gap-x-2">
-                <div class="w-20 h-4 rounded-md bg-gray-200/75"></div>
-                <div class="w-20 h-4 rounded-md bg-gray-200/75"></div>
+    <div class="max-w-[405px] w-full">
+        <div class="h-[230px] bg-gray-200/75 rounded-lg"/>
+            <div class="flex mt-2 justify-between gap-x-2">
+                <div class="flex gap-x-2">
+                    <div class="w-20 h-4 rounded-md bg-gray-200/75"/>
+                    <div class="w-20 h-4 rounded-md bg-gray-200/75"/>
+                </div>
+                <div class="w-20 h-4 rounded-md bg-gray-200/75"/>
             </div>
-            <div class="w-20 h-4 rounded-md bg-gray-200/75"></div>
-        </div>
-        <div class="mt-5 w-full h-4 rounded-md bg-gray-200/75"></div>
-        <div class="mt-2 w-full h-4 rounded-md bg-gray-200/75"></div>
+            <div class="mt-5 w-full h-4 rounded-md bg-gray-200/75" />
+            <div class="mt-2 w-full h-4 rounded-md bg-gray-200/75" />
     </div>
 </template>
