4d7a097bd78c2f9b945e41b70106af089f9cc062
Elmir Mammadli <elmirmammadli18@gmail.com>
2025-04-12 17:07:42 -0400

    Update blog post page to use caching and meta tags

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
 const route = useRoute()
-console.log('Route', route.params);
+const { getStory } = useStoryblokCache()
+const { setBlogMeta } = useMetaTags()
 
-const story = await useAsyncStoryblok(
-  `blog/${Array.isArray(route.params.slug) ? route.params.slug.join('/') : route.params.slug}`, 
-  { version: 'draft' }
+// Generate the slug path
+const slugPath = Array.isArray(route.params.slug) 
+  ? route.params.slug.join('/') 
+  : route.params.slug
+
+// Get the story with caching
+const story = await getStory(
+  `blog/${slugPath}`, 
+  { version: 'draft' },
+  3600 * 1000 // Cache for 1 hour
 )
+
+// Set meta tags for the blog post
+setBlogMeta({
+  title: story.name,
+  description: story.content?.description || story.content?.perex,
+  image: story.content?.image,
+  author: story.content?.author,
+  publishDate: story.first_published_at,
+  fullSlug: story.full_slug,
+  category: Array.isArray(route.params.slug) && route.params.slug.length > 0 
+    ? route.params.slug[0] 
+    : undefined
+})
 </script>
+
 <template>
-    <div>
+    <div class="mx-auto max-w-screen-rD">
         <StoryblokComponent ref="target" :blok="story.content" :name="story.name" />
     </div>
 </template>
